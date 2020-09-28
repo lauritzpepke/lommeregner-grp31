4544364712be6f5640f9f0d799cf7c5a7248c3d6
Aqibsen <71653438+Aqibsen@users.noreply.github.com>
2020-09-28 14:31:01 +0200

    Ændring i regnemaskine udregning
    Ændring af + til *

---
+++ Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner Lauritz = new Scanner(System.in);
+        int første, andet, svar;
+        System.out.println("Skriv første nummer: ");
+        første = Lauritz.nextInt();
+        System.out.println("skriv andet nummer: ");
+        andet = Lauritz.nextInt();
+        svar = første * andet;
+        System.out.println(svar);
+
+    }
+}
+
